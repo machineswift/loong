78c76ff6104a4a60e667a2422847f2ba8708d780
machineswift <machineswift@gmail.com>
2023-12-13 01:15:26 +0800

    【建工程】init dragon-app-manage

---
+++ dragon-app/dragon-app-manage/src/test/java/com/machine/app/manage/DragonManageAppTests.java
@@ -0,0 +1,13 @@
+package com.machine.app.manage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DragonManageAppTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
