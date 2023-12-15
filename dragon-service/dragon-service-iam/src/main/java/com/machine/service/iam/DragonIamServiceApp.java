d15665d6b8417157d0e656802e5c7798b17fc561
machineswift <machineswift@gmail.com>
2023-12-15 10:38:50 +0800

    【建工程】init dragon-service-iam

---
+++ dragon-service/dragon-service-iam/src/main/java/com/machine/service/iam/DragonIamServiceApp.java
@@ -0,0 +1,13 @@
+package com.machine.service.iam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DragonIamServiceApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DragonIamServiceApp.class, args);
+    }
+
+}
---
+++ dragon-service/dragon-service-iam/src/test/java/com/machine/service/iam/DragonIamServiceAppTests.java
@@ -0,0 +1,13 @@
+package com.machine.service.iam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DragonIamServiceAppTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
