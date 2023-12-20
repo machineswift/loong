bbdf4374191dfdfe782d88eeaf24f8bda0dc991b
machineswift <machineswift@gmail.com>
2023-12-20 23:23:11 +0800

    【建工程】spring security json 登录

--- dragon-app/dragon-app-iam/src/main/java/com/machine/app/iam/config/SpringSecurityConfig.java
+++ dragon-app/dragon-app-iam/src/main/java/com/machine/app/iam/config/SpringSecurityConfig.java
@@ -1,13 +1,14 @@
 package com.machine.app.iam.config;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.machine.app.iam.authentication.CustomAuthenticationEntryPoint;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.csrf.HttpSessionCsrfTokenRepository;
 
 import java.io.PrintWriter;
 
@@ -17,25 +18,63 @@ public class SpringSecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests(authorize -> authorize.requestMatchers("/public/**").permitAll().anyRequest().authenticated()).formLogin(formLogin -> formLogin
-                //.loginPage("/login")
-                //.loginProcessingUrl("/login")
-                .successHandler((req, resp, authentication) -> {
-                    Object principal = authentication.getPrincipal();
-                    resp.setContentType("application/json;charset=utf-8");
-                    PrintWriter out = resp.getWriter();
-                    out.write(new ObjectMapper().writeValueAsString(principal));
-                    out.flush();
-                    out.close();
-                }).failureHandler((req, resp, e) -> {
-                    resp.setContentType("application/json;charset=utf-8");
-                    PrintWriter out = resp.getWriter();
-                    out.write(e.getMessage());
-                    out.flush();
-                    out.close();
-                }).permitAll()).exceptionHandling(exceptionHandle ->
-                exceptionHandle.authenticationEntryPoint(new CustomAuthenticationEntryPoint())).rememberMe(Customizer.withDefaults());
+        http.
+                authorizeHttpRequests(authorize ->
+                        authorize.requestMatchers("/public/**")
+                                .permitAll()
+                                .anyRequest()
+                                .authenticated())
+                .formLogin(formLogin ->
+                        formLogin.loginProcessingUrl("/doLogin")
+                                .usernameParameter("username")
+                                .passwordParameter("password")
+                                .successHandler((req, resp, authentication) -> {
+                                    Object principal = authentication.getPrincipal();
+                                    resp.setContentType("application/json;charset=utf-8");
+                                    PrintWriter out = resp.getWriter();
+                                    out.write(new ObjectMapper().writeValueAsString(principal));
+                                    out.flush();
+                                    out.close();
+                                })
+                                .failureHandler((req, resp, e) -> {
+                                    resp.setContentType("application/json;charset=utf-8");
+                                    PrintWriter out = resp.getWriter();
+                                    out.write(e.getMessage());
+                                    out.flush();
+                                    out.close();
+                                }).permitAll())
 
+                .logout(logout ->
+                        logout.logoutUrl("/logout")
+                                .logoutSuccessHandler((req, resp, authentication) -> {
+                                    resp.setContentType("application/json;charset=utf-8");
+                                    PrintWriter out = resp.getWriter();
+                                    out.write("注销成功");
+                                    out.flush();
+                                    out.close();
+                                }))
+                .exceptionHandling(exceptionHandle ->
+                        exceptionHandle
+                                .accessDeniedHandler((req, resp, e) -> {
+                                    resp.setContentType("application/json;charset=utf-8");
+                                    PrintWriter out = resp.getWriter();
+                                    out.write(e.getMessage());
+                                    out.flush();
+                                    out.close();
+                                })
+                                .authenticationEntryPoint((req, resp, e) -> {
+                                    resp.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Unauthorized");
+                                    resp.setContentType("application/json;charset=utf-8");
+                                    PrintWriter out = resp.getWriter();
+                                    out.write("没有登录");
+                                    out.flush();
+                                    out.close();
+                                })
+                )
+                .csrf((csrf) -> csrf
+                        .csrfTokenRepository(new HttpSessionCsrfTokenRepository()).disable()
+                )
+                .rememberMe(Customizer.withDefaults());
         return http.build();
     }
 
