dbaf70ae130716004247480664108e341aab65c0
machineswift <machineswift@gmail.com>
2024-01-31 22:18:43 +0800

    【建工程】area 代码调整

--- dragon-service/dragon-service-data/src/main/java/com/machine/service/data/DragonDataServiceApp.java
+++ dragon-service/dragon-service-data/src/main/java/com/machine/service/data/DragonDataServiceApp.java
@@ -1,11 +1,9 @@
 package com.machine.service.data;
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@MapperScan("com.machine.service.data.area.mapper")
 public class DragonDataServiceApp {
 
     public static void main(String[] args) {
