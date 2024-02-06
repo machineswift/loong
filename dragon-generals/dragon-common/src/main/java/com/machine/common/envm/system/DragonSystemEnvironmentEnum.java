54f7df59627a958ebfb58981de67f8265d8ef41d
machineswift <machineswift@gmail.com>
2024-02-06 19:51:51 +0800

    【data】标签

---
+++ dragon-generals/dragon-common/src/main/java/com/machine/common/envm/system/DragonSystemEnvironmentEnum.java
@@ -0,0 +1,16 @@
+package com.machine.common.envm.system;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum DragonSystemEnvironmentEnum {
+    DEV("DEV", "开发环境"),
+    TEST("TEST", "测试环境"),
+    UAT("UAT", "预发布环境"),
+    PROD("PROD", "生产环境");
+
+    private final String code;
+    private final String msg;
+}
