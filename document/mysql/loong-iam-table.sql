f327ddacd35afadb39f97816da10f75ad2b8ffb2
machineswift <machineswift@gmail.com>
2024-03-15 16:23:00 +0800

    【loong-iam-service】iam表

--- document/mysql/loong-iam-table.sql
+++ document/mysql/loong-iam-table.sql
@@ -69,12 +69,12 @@ CREATE TABLE `t_group` (
 ) COMMENT = '分组表';
 
 
-DROP TABLE IF EXISTS `t_role_permission`;
-CREATE TABLE `t_role_permission` (
+DROP TABLE IF EXISTS `t_role_permission_relation`;
+CREATE TABLE `t_role_permission_relation` (
    `id` VARCHAR ( 32 ) NOT NULL COMMENT 'ID',
    `role_id` VARCHAR ( 32 ) NOT NULL COMMENT '角色ID',
 	`permission_id` VARCHAR ( 32 ) NOT NULL COMMENT '权限id',
-	`type` INT NOT NULL COMMENT '描述',
+	`type` INT NOT NULL COMMENT '权限类型,（0:可访问，1:可授权）',
 	`create_by` VARCHAR ( 32 ) NOT NULL COMMENT '创建人',
 	`create_time` BIGINT UNSIGNED NOT NULL COMMENT '创建时间',
 	`update_by` VARCHAR ( 32 ) NOT NULL COMMENT '修改人',
@@ -82,15 +82,15 @@ CREATE TABLE `t_role_permission` (
 	`deleted` TINYINT UNSIGNED NOT NULL DEFAULT '0' COMMENT '是否已删除',
 	PRIMARY KEY ( `id` ) USING BTREE,
     UNIQUE KEY `uk_01` ( `role_id`,`permission_id` ) USING BTREE
-) COMMENT = '角色权限表';
-DROP TABLE IF EXISTS `t_role_permission`;
+) COMMENT = '角色权限关系表';
 
-DROP TABLE IF EXISTS `t_group_permission`;
-CREATE TABLE `t_group_permission` (
+
+DROP TABLE IF EXISTS `t_group_permission_relation`;
+CREATE TABLE `t_group_permission_relation` (
    `id` VARCHAR ( 32 ) NOT NULL COMMENT 'ID',
-   `group_id` VARCHAR ( 32 ) NOT NULL COMMENT '组ID',
+   `group_id` VARCHAR ( 32 ) NOT NULL COMMENT '分组ID',
 	`permission_id` VARCHAR ( 32 ) NOT NULL COMMENT '权限id',
-	`type` INT NOT NULL COMMENT '描述',
+	`type` INT NOT NULL COMMENT '权限类型,（0:可访问，1:可授权）',
 	`create_by` VARCHAR ( 32 ) NOT NULL COMMENT '创建人',
 	`create_time` BIGINT UNSIGNED NOT NULL COMMENT '创建时间',
 	`update_by` VARCHAR ( 32 ) NOT NULL COMMENT '修改人',
@@ -98,4 +98,116 @@ CREATE TABLE `t_group_permission` (
 	`deleted` TINYINT UNSIGNED NOT NULL DEFAULT '0' COMMENT '是否已删除',
 	PRIMARY KEY ( `id` ) USING BTREE,
     UNIQUE KEY `uk_01` ( `group_id`,`permission_id` ) USING BTREE
-) COMMENT = '角色权限表';
+) COMMENT = '分组权限关系表';
+
+
+DROP TABLE IF EXISTS `t_group_role_relation`;
+CREATE TABLE `t_group_role_relation` (
+   `id` VARCHAR ( 32 ) NOT NULL COMMENT 'ID',
+   `group_id` VARCHAR ( 32 ) NOT NULL COMMENT '分组ID',
+	`role_id` VARCHAR ( 32 ) NOT NULL COMMENT '角色id',
+	`create_by` VARCHAR ( 32 ) NOT NULL COMMENT '创建人',
+	`create_time` BIGINT UNSIGNED NOT NULL COMMENT '创建时间',
+	`update_by` VARCHAR ( 32 ) NOT NULL COMMENT '修改人',
+	`update_time` BIGINT UNSIGNED NOT NULL COMMENT '更新时间',
+	`deleted` TINYINT UNSIGNED NOT NULL DEFAULT '0' COMMENT '是否已删除',
+	PRIMARY KEY ( `id` ) USING BTREE,
+    UNIQUE KEY `uk_01` ( `group_id`,`role_id` ) USING BTREE
+) COMMENT = '分组角色关系表';
+
+
+DROP TABLE IF EXISTS `t_user_group_relation`;
+CREATE TABLE `t_user_group_relation` (
+   `id` VARCHAR ( 32 ) NOT NULL COMMENT 'ID',
+   `user_id` VARCHAR ( 32 ) NOT NULL COMMENT '角色id',
+   `group_id` VARCHAR ( 32 ) NOT NULL COMMENT '组ID',
+	`create_by` VARCHAR ( 32 ) NOT NULL COMMENT '创建人',
+	`create_time` BIGINT UNSIGNED NOT NULL COMMENT '创建时间',
+	`update_by` VARCHAR ( 32 ) NOT NULL COMMENT '修改人',
+	`update_time` BIGINT UNSIGNED NOT NULL COMMENT '更新时间',
+	`deleted` TINYINT UNSIGNED NOT NULL DEFAULT '0' COMMENT '是否已删除',
+	PRIMARY KEY ( `id` ) USING BTREE,
+    UNIQUE KEY `uk_01` ( `user_id`,`group_id` ) USING BTREE
+) COMMENT = '用户分组关系表'
+
+
+DROP TABLE IF EXISTS `t_user_permission_relation`;
+CREATE TABLE `t_user_permission_relation` (
+   `id` VARCHAR ( 32 ) NOT NULL COMMENT 'ID',
+   `user_id` VARCHAR ( 32 ) NOT NULL COMMENT '用户ID',
+	`permission_id` VARCHAR ( 32 ) NOT NULL COMMENT '权限id',
+	`type` INT NOT NULL COMMENT '权限类型,（0:可访问，1:可授权）',
+	`create_by` VARCHAR ( 32 ) NOT NULL COMMENT '创建人',
+	`create_time` BIGINT UNSIGNED NOT NULL COMMENT '创建时间',
+	`update_by` VARCHAR ( 32 ) NOT NULL COMMENT '修改人',
+	`update_time` BIGINT UNSIGNED NOT NULL COMMENT '更新时间',
+	`deleted` TINYINT UNSIGNED NOT NULL DEFAULT '0' COMMENT '是否已删除',
+	PRIMARY KEY ( `id` ) USING BTREE,
+    UNIQUE KEY `uk_01` ( `user_id`,`permission_id` ) USING BTREE
+) COMMENT = '用户权限关系表';
+
+
+DROP TABLE IF EXISTS `t_user_role_relation`;
+CREATE TABLE `t_user_role_relation` (
+   `id` VARCHAR ( 32 ) NOT NULL COMMENT 'ID',
+   `user_id` VARCHAR ( 32 ) NOT NULL COMMENT '用户ID',
+	`role_id` VARCHAR ( 32 ) NOT NULL COMMENT '角色id',
+	`create_by` VARCHAR ( 32 ) NOT NULL COMMENT '创建人',
+	`create_time` BIGINT UNSIGNED NOT NULL COMMENT '创建时间',
+	`update_by` VARCHAR ( 32 ) NOT NULL COMMENT '修改人',
+	`update_time` BIGINT UNSIGNED NOT NULL COMMENT '更新时间',
+	`deleted` TINYINT UNSIGNED NOT NULL DEFAULT '0' COMMENT '是否已删除',
+	PRIMARY KEY ( `id` ) USING BTREE,
+    UNIQUE KEY `uk_01` ( `user_id`,`role_id` ) USING BTREE
+) COMMENT = '用户角色关系表';
+
+
+
+DROP TABLE IF EXISTS `t_organization`;
+CREATE TABLE `t_organization` (
+   `id` VARCHAR ( 32 ) NOT NULL COMMENT 'ID',
+   `parent_id` VARCHAR ( 32 ) NOT NULL COMMENT '父ID',
+	`name` VARCHAR ( 32 ) NOT NULL COMMENT '用户名',
+	`description` VARCHAR ( 2048 ) NOT NULL COMMENT '描述',
+	`create_by` VARCHAR ( 32 ) NOT NULL COMMENT '创建人',
+	`create_time` BIGINT UNSIGNED NOT NULL COMMENT '创建时间',
+	`update_by` VARCHAR ( 32 ) NOT NULL COMMENT '修改人',
+	`update_time` BIGINT UNSIGNED NOT NULL COMMENT '更新时间',
+	`deleted` TINYINT UNSIGNED NOT NULL DEFAULT '0' COMMENT '是否已删除',
+	PRIMARY KEY ( `id` ) USING BTREE,
+    KEY `idx_01` ( `parent_id` ) USING BTREE,
+	KEY `idx_02` ( `create_time` ) USING BTREE
+) COMMENT = '组织表';
+
+
+
+DROP TABLE IF EXISTS `t_user_organization_relation`;
+CREATE TABLE `t_user_organization_relation` (
+   `id` VARCHAR ( 32 ) NOT NULL COMMENT 'ID',
+   `user_id` VARCHAR ( 32 ) NOT NULL COMMENT '用户ID',
+	`organization_id` VARCHAR ( 32 ) NOT NULL COMMENT '组织id',
+	`create_by` VARCHAR ( 32 ) NOT NULL COMMENT '创建人',
+	`create_time` BIGINT UNSIGNED NOT NULL COMMENT '创建时间',
+	`update_by` VARCHAR ( 32 ) NOT NULL COMMENT '修改人',
+	`update_time` BIGINT UNSIGNED NOT NULL COMMENT '更新时间',
+	`deleted` TINYINT UNSIGNED NOT NULL DEFAULT '0' COMMENT '是否已删除',
+	PRIMARY KEY ( `id` ) USING BTREE,
+    UNIQUE KEY `uk_01` ( `user_id`,`organization_id` ) USING BTREE
+) COMMENT = '用户组织关系表';
+
+
+DROP TABLE IF EXISTS `t_user_operate_log`;
+CREATE TABLE `t_user_operate_log` (
+   `id` VARCHAR ( 32 ) NOT NULL COMMENT 'ID',
+   `user_id` VARCHAR ( 32 ) NOT NULL COMMENT '用户ID',
+	`name` VARCHAR ( 32 ) NOT NULL COMMENT '用户名',
+	`description` VARCHAR ( 2048 ) NOT NULL COMMENT '描述',
+	`create_by` VARCHAR ( 32 ) NOT NULL COMMENT '创建人',
+	`create_time` BIGINT UNSIGNED NOT NULL COMMENT '创建时间',
+	`update_by` VARCHAR ( 32 ) NOT NULL COMMENT '修改人',
+	`update_time` BIGINT UNSIGNED NOT NULL COMMENT '更新时间',
+	`deleted` TINYINT UNSIGNED NOT NULL DEFAULT '0' COMMENT '是否已删除',
+	PRIMARY KEY ( `id` ) USING BTREE,
+    KEY `idx_01` ( `parent_id` ) USING BTREE,
+	KEY `idx_02` ( `create_time` ) USING BTREE
+) COMMENT = '用户操作日志表';
