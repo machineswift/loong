db34781e55aa158a45b5d7f1c951a018f098ba7a
machineswift <machineswift@gmail.com>
2024-08-30 15:41:55 +0800

    【loong】loong-hrm-service

---
+++ loong-services/loong-hrm-service/src/main/java/com/machine/service/hrm/LoongHrmServiceApp.java
@@ -0,0 +1,13 @@
+package com.machine.service.hrm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoongHrmServiceApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LoongHrmServiceApp.class, args);
+    }
+
+}
