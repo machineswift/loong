cae7bdad11703b3f5d95bd076586a521a8bd799f
machineswift <machineswift@gmail.com>
2024-09-03 15:20:09 +0800

    【loong】 loong-security-starter

--- loong-starters/loong-security-boot-starter/src/main/java/com/machine/starter/security/SecurityAutoConfiguration.java
+++ loong-starters/loong-security-boot-starter/src/main/java/com/machine/starter/security/SecurityAutoConfiguration.java
@@ -3,10 +3,8 @@ package com.machine.starter.security;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
-
 @Configuration(proxyBeanMethods = false)
 @EnableConfigurationProperties({LoongSecurityProperties.class})
 public class SecurityAutoConfiguration {
-
 
 }
