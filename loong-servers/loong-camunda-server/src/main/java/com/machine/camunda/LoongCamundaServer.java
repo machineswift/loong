0145545a1589442bd452877f17114ede3612b635
machineswift <machineswift@gmail.com>
2024-09-14 08:59:50 +0800

    【loong】 loong-camunda-server

---
+++ loong-servers/loong-camunda-server/src/main/java/com/machine/camunda/LoongCamundaServer.java
@@ -0,0 +1,13 @@
+package com.machine.camunda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoongCamundaServer {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LoongCamundaServer.class, args);
+    }
+
+}
